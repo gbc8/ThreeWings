f753da425e695b409c797bdd4ef058995e0aa0b0
gbc8 <gbc7@cin.ufpe.br>
2023-12-16 15:02:59 -0300

    Configuração inicial Spring e Docker

---
+++ produto/src/test/java/com/gbc8/produto/ProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbc8.produto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
